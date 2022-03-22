1da37037ecc9776eee320f2796a65ec34e7a691b
Martin Dousa <martindousa186@gmail.com>
2022-03-22 01:40:56 +0100

    ...

--- premake5.lua
+++ premake5.lua
@@ -1,42 +1,39 @@
 project "ImGui"
-  kind "StaticLib"
-  language "C++"
-  cppdialect "C++20"
-  staticruntime "on"
+	kind "StaticLib"
+	language "C++"
+  systemversion "latest"
+	cppdialect "C++17"
+	staticruntime "On"
 
-  targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-  objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-  files
-  {
-    "imconfig.h",
-    "imgui.h",
-    "imgui.cpp",
-    "imgui_draw.cpp",
-    "imgui_internal.h",
+	files
+	{
     "imgui_tables.cpp",
-    "imgui_widgets.cpp",
-    "imstb_rectpack.h",
-    "imstb_textedit.h",
-    "imstb_truetype.h",
-    "imgui_demo.cpp"
-  }
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
 
-  filter "system:windows"
-    systemversion "latest"
+	filter "system:linux"
+		pic "On"
 
-  filter "system:linux"
-    pic "On"
-    systemversion "latest"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
 
-  filter "configurations:Debug"
-    runtime "Debug"
-    symbols "on"
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
 
-  filter "configurations:Release"
-    runtime "Release"
-    optimize "on"
-
-  filter "configurations:Dist"
-    runtime "Release"
-    optimize "on"
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
